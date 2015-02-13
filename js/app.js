77547941b83c129b23fd66f5634a31be0c1f22b1
davidhoos <david.c.hoos@gmail.com>
2015-02-13 09:44:14 -0800

    simplified

--- js/app.js
+++ js/app.js
@@ -1,73 +1,7 @@
 $(document).ready(function() {
-
-   $('#userInput').keydown(enterItem);
-    $('.add').click(clickItem);
-
-function clickItem() {
-
-    userInput = $("#userInput").val();
-
-    if (userInput === '') {
-        return;
-
-    } else {
-
-        todoItem = '<li class="to-do-wrapper"><input type="text" class="to-do item" value="' + userInput + '" readonly><span class="userAction submit complete-item"><i class="' + unselectedClass + '"></i></span></li>';
-
-        $(todoItem).appendTo('.to-do-items-wrapper').slideDown();
-
-        $("#userInput").val('');
-    }
-}
-
-function enterItem() {
- 
-    $("#userInput").keypress(function(event) {
-
-        if (event.keyCode == 13) {
-            event.preventDefault();
-
-            $(".add").click();
-        }
-    });
-}
-
-function completeItem() {
-    $(this).find('.userAction').parent().toggleClass('completed');
-
-
-    if ($(this).closest('.to-do-wrapper').hasClass('completed')) {
-        $(this).find('.userAction i').removeClass(unselectedClass).addClass(selectedClass);
-    } else {
-        $(this).find('.userAction i').removeClass(selectedClass).addClass(unselectedClass);
-    }
-
-    if ($('.to-do-wrapper').hasClass('completed')) {
-        $('.delete-completed').slideDown();
-    } else {
-        $('.delete-completed').slideUp();
-    }
-}
-
-
-function deleteCompleted() {
-
-    $(this).closest('.wrapper').find('.to-do-items-wrapper').children('.completed').slideUp(function() {
-        $(this).remove();
-    });
-}
- 
-$('.delete-completed').on('mouseenter', function() {
-    $('.completed').addClass('about-to-delete');
+    $('#listbutton').click(function(){
+        $('#unorderedlist').append($('<li>', {
+        text: $('#inputtext').val()
+        }))
+    })
 });
-
-$('.delete-completed').on('mouseleave', function() {
-    $('.completed').removeClass('about-to-delete');
-});
-
-$('.delete-completed').click(deleteCompleted);
-$('.to-do-items-wrapper').on('click', '.to-do-wrapper', completeItem);
-});
-
-var selectedClass = 'Check';
-var unselectedClass = 'Uncheck';
